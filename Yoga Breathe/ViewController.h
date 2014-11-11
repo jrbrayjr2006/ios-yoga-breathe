f5dc0ce4f28cb794dc9a50c509b65a25aa74edbb
James Bray <jrbrayjr2006@gmail.com>
2014-11-11 14:18:36 -0600

    Initial Commit

---
+++ Yoga Breathe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Yoga Breathe
+//
+//  Created by JAMES BRAY on 11/11/14.
+//  Copyright (c) 2014 Jaydot2 LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Yoga Breathe/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Yoga Breathe
+//
+//  Created by JAMES BRAY on 11/11/14.
+//  Copyright (c) 2014 Jaydot2 LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
